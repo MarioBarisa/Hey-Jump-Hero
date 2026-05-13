5428145db783070a1ba7655fdc37abc2084b0f98
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2026-05-13 19:28:44 +0200

    sund effect za shield dodan

--- Assets/Skripte/ShieldPowerup.cs
+++ Assets/Skripte/ShieldPowerup.cs
@@ -6,6 +6,8 @@ public class ShieldPowerup : MonoBehaviour
     [SerializeField] float duration = 20f;
     [SerializeField] float damageReduction = 0.3f;
 
+    [SerializeField] private AudioSource audioSource;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter2D(Collider2D other)
@@ -16,6 +18,7 @@ public class ShieldPowerup : MonoBehaviour
             if (healthSystem != null)
             {
                 healthSystem.ActivateShield(duration, damageReduction);
+                AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
                 Destroy(gameObject);
             }
         }
