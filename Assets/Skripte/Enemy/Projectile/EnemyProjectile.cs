2d665a89a0c27d3a5eef91b217e244bcf16364e4
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2026-06-01 13:35:03 +0200

    dodan enemy koji puca projektile (ne radi još)

---
+++ Assets/Skripte/Enemy/Projectile/EnemyProjectile.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float direction;
+    private bool hit;
+
+    private BoxCollider2D boxCollider;
+
+    public void Awake()
+    {
+        boxCollider= GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if(hit) return;
+
+        float movementSpeed= speed * Time.deltaTime * direction;
+        transform.Translate(movementSpeed, 0, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit= true;
+        boxCollider.enabled= false;
+
+    }
+
+    public void SetDirection(float _direction)
+    {
+        direction= _direction;
+        gameObject.SetActive(true);
+        hit= false;
+        boxCollider.enabled= true;
+
+        float localScaleX= transform.localScale.x;
+        if(Mathf.Sign(localScaleX)!= _direction)
+        {
+            localScaleX= -localScaleX;
+        }
+        transform.localScale= new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Dectivate()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Skripte/Enemy/RangedEnemy.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class RangedEnemy : MonoBehaviour
+{
+    [Header("Attack Parameters")]
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float damage;
+
+    [Header("Ranged Attack")]
+    [SerializeField] private Transform projectilePoint;
+    [SerializeField] private GameObject[] projectiles;
+    
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer= Mathf.Infinity;
+
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        if (PlayerInSight())
+        {
+            if(cooldownTimer >= attackCooldown)
+            {
+                RangedAttack();
+            }
+        }        
+    }
+
+
+    private void RangedAttack()
+    {
+        cooldownTimer= 0;
+
+        int projectileIndex= FindProjectile();
+
+
+        projectiles[projectileIndex].transform.position= projectilePoint.position;
+        projectiles[projectileIndex].GetComponent<EnemyProjectile>().SetDirection(transform.localScale.x);
+        
+    }
+
+    private int FindProjectile()
+    {
+        for(int i=0; i<projectiles.Length; i++)
+        {
+            if (!projectiles[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit= Physics2D.BoxCast(
+            boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(
+                boxCollider.bounds.size.x * range,
+                boxCollider.bounds.size.y,
+                boxCollider.bounds.size.z
+            ),
+            0,
+            Vector2.left,
+            0,
+            playerLayer
+        );
+        return hit.collider!= null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color= Color.red;
+        Gizmos.DrawWireCube(
+            boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(
+                boxCollider.bounds.size.x * range,
+                boxCollider.bounds.size.y,
+                boxCollider.bounds.size.z
+            )
+        );
+    }
+
+}
